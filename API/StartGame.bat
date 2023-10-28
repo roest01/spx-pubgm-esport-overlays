c603666d78e905afc866496ba41437cd07eabab6
Tobias Rös <roes@amicaldo.de>
2023-10-28 16:56:46 +0200

    add bat files to quickstart api / game or both

---
+++ API/StartGame.bat
@@ -0,0 +1,1 @@
+start C:\Users\REPLACE_THIS_PATH\pcob_folder\WindowsNoEditor\ShadowTrackerExtra\Binaries\Win64\ShadowTrackerExtra.exe /Game/Maps/UImap/Editor_login -log
---
+++ API/StartGameAndAPI.bat
@@ -0,0 +1,3 @@
+start C:\Users\REPLACE_THIS_PATH\pcob_folder\WindowsNoEditor\ShadowTrackerExtra\Binaries\Win64\ShadowTrackerExtra.exe /Game/Maps/UImap/Editor_login -log
+
+node.exe ob.js
